1ec09d02a08f3fd497d61fd44424556acad19e4b
norrico31 <norricobiason31@gmail.com>
2023-11-05 14:06:33 +0800

    dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,10 +3,17 @@ import { NavLink, Outlet, useLocation } from "react-router-dom";
 import { Button } from '../components'
 
 export default function Dashboard() {
+    return <Container>
+        <Main />
+        <Outlet />
+    </Container>
+}
+
+function Main() {
     let { pathname } = useLocation()
     pathname = pathname.includes('critical') ? 'critical-equipment' : pathname
-    return <Container fluid className='bg-color-white py-4'>
-        <Row className='justify-content-between gap-2 align-items-center p-0 m-0'>
+    return <>
+        <Row className='justify-content-between gap-2 align-items-center'>
             <Col>
                 <h3>{heading[shortenPathname(pathname)]}</h3>
             </Col>
@@ -14,13 +21,10 @@ export default function Dashboard() {
         </Row>
         <hr />
         <div className='text-end mb-4'>
-            <Button variant='primary' className='btn-print-report text-end' title={`Print Rerport - aha`}>Print Report - SWP</Button>
+            <Button variant='primary' className='btn-print-report text-end' title={`Print Rerport - aha`}>Print Report - {heading[shortenPathname(pathname)]}</Button>
         </div>
-        <Outlet />
-    </Container>
+    </>
 }
-
-const shortenPathname = (path: string) => path.includes('-') ? path.split('-')[0] : path.split('/')[2]
 
 function NavLists() {
     return links.map((l, idx) => (
@@ -36,6 +40,8 @@ const heading: Record<string, string> = {
     pipelines: 'Pipelines',
     critical: 'Critical Equipment',
 }
+
+const shortenPathname = (path: string) => path.includes('-') ? path.split('-')[0] : path.split('/')[2]
 
 const links = [
     {
