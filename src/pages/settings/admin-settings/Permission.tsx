c3b22d3bf53dff87f523b10e46b6a0e8b663a347
norrico31 <norricobiason31@gmail.com>
2023-11-10 08:36:04 +0800

    permission module

--- src/pages/settings/admin-settings/Permission.tsx
+++ src/pages/settings/admin-settings/Permission.tsx
@@ -1,7 +1,54 @@
+import { Card, List, Switch } from 'antd'
 import React from 'react'
+
+const data = [
+    {
+        title: 'Dashboard',
+    },
+    {
+        title: 'Users Management',
+    },
+
+]
 
 export default function Permission() {
     return (
-        <div>Permission</div>
+        <div>
+            <h3 className='text-color-gray mb-2'>Permissions</h3>
+            <List
+                grid={{
+                    gutter: 16,
+                    xs: 1,
+                    sm: 2,
+                    md: 2,
+                    lg: 3,
+                    xl: 3,
+                    xxl: 3,
+                }}
+                dataSource={data}
+                renderItem={(item) => (
+                    <List.Item>
+                        <Card title={item.title}>
+                            <div className='d-flex justify-content-between'>
+                                <p>Create</p>
+                                <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />
+                            </div>
+                            <div className='d-flex justify-content-between'>
+                                <p>Edit</p>
+                                <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />
+                            </div>
+                            <div className='d-flex justify-content-between'>
+                                <p>Delete</p>
+                                <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />
+                            </div>
+                            <div className='d-flex justify-content-between'>
+                                <p>View</p>
+                                <Switch checkedChildren="Yes" unCheckedChildren="No" defaultChecked />
+                            </div>
+                        </Card>
+                    </List.Item>
+                )}
+            />
+        </div>
     )
 }
